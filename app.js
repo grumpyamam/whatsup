95385870cb3d7ea0cce739e6f952f5abbf13f564
Tarik SIDI MAMMAR <Tarik SIDI MAMMAR>
2016-12-18 01:16:22 +0000

    add some npm to reload web browser + nodemon to hot update react + some webpack loaders to manage bootstrap

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var path = require('path');
+var reload = require('reload');
+
+var publicDir = path.join(__dirname, 'public');
+ 
+var server = http.createServer(app);
+
+ 
+app.use(express.static('src/client'));
+
+app.get('/', function (req, res) {
+  //res.send('Hello Worrgrrrrllld!!!');
+  res.sendFile(path.join(__dirname, 'src/client', 'index.html'));
+})
+
+reload(server, app);
+
+/*app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+})*/
+
+server.listen(3000, function(){
+  console.log("Web server listening on port " + 3000);
+  
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,15 @@ var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
+var STYLE_DIR = path.resolve(__dirname, 'src/client/app/style');
 var TEST_DIR = path.resolve(__dirname, 'src/client/test');
 
 
 var config = {
   entry: {
-	  test: TEST_DIR + '/index.jsx'/*,
+	  test: TEST_DIR + '/index.jsx',
 	  app: 	APP_DIR + '/index.jsx'
-	  */
+	  
   },
   output: {
     path: BUILD_DIR,
@@ -21,9 +22,28 @@ var config = {
     loaders : [
       {
         test : /\.jsx?/,
-        include : TEST_DIR, /*APP_DIR*/
+        include : APP_DIR, /*TEST_DIR*/
+        loader : 'babel'
+      },
+	  { 
+		test: /\.css$/, 
+		loader: "style!css"
+	  },
+	  /*{
+		test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+		loader: 'file-loader'	
+	  },*/	  
+	  /*this part is for test*/
+	  {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
+	  {test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000'},
+	  {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+	  {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'},
+      {
+        test : /\.jsx?/,
+        include : TEST_DIR,
         loader : 'babel'
       }
+	  
     ]
   }
 };
